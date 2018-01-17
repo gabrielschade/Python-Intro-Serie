7b8f5fb07d17907bd877b6bdd2f1045e8fdff8d4
Gabriel Schade <re035148@ITAW10N07304>
2018-01-17 11:51:12 -0200

    Commit dos primeiros exercícios

---
+++ 01-VariaveisTiposIO.py
@@ -0,0 +1,19 @@
+"""Faça um algoritmo para resolver a equação z = x + y - 4, 
+armazenando o resultado na variável z. 
+Considere que os valores iniciais de x e y são, 
+respectivamente, 10 e 5. Depois disso exiba o resultado."""
+x = 10
+y = 5
+z = x + y - 4
+print(z)
+
+"""Faça um algoritmo para resolver a equação z = a * a - b + c, 
+armazenando o resultado na variável z.
+Considere que os valores iniciais de a, b e c são, 
+respecitvamente, 4, 6 e 10. Depois disso exiba o resultado."""
+
+a = 4
+b = 6
+c = 10
+z = a * a - b + c
+print(z)
