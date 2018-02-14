2b1f414203d3a4c4b5edcb6f592e0d5d7a3bd85c
Gabriel Schade <re035148@ITAW10N07304>
2018-02-14 16:57:19 -0200

    Inclusão do arquivo para exercícios com lista

---
+++ 04-Listas.py
@@ -0,0 +1,24 @@
+""" Lista vazia"""
+lista_de_compras = []
+
+""" Lista com itens"""
+lista_de_compras = ["Pão", "Água", "Leite", "Ovos"]
+
+print(lista_de_compras[0])
+print(lista_de_compras[1])
+print(lista_de_compras[2])
+print(lista_de_compras[3])
+
+print(lista_de_compras[-1])
+print(lista_de_compras[-2])
+print(lista_de_compras[-3])
+print(lista_de_compras[-4])
+
+""" Alterando um elemento da lista """
+
+lista_de_compras[2] = "Suco de Laranja"
+
+""" Adicionando um item na lista """
+
+lista_de_compras.append("Café")
+print(lista_de_compras[4])
